773f389276ff4bbbd9191f53eb7fa88ab6ab67a3
Andres Fonseca <theandresfonseca@gmail.com>
2017-01-03 17:17:59 +0000

    new ui indentation

--- ui.r
+++ ui.r
@@ -23,37 +23,45 @@ fluidPage(
   
   fluidRow(
     column(8, offset = 2, align = 'center',
-           h1('W A V E S'),
-           bsCollapse(id = "main", open = "intro",
-                      bsCollapsePanel('intro',
-                                      img(src = 'eddie-intro3.jpg', align = 'center', width = '100%', height = 'auto')
-                      ),
-                      bsCollapsePanel('1. graphic size',
-                                      p('select between an across the chest graphic (11"x6") or a left pocket icon (3"x1.6")'),
-                                      radioButtons_withHTML('image_size', NULL, choices = image_sizes, inline = TRUE)
-                      ),
-                      bsCollapsePanel('2. create',
-                                      tabsetPanel(
-                                        tabPanel('record',
-                                                 fluidRow(
-                                                   imageOutput('sample_wave', height = 'auto', width = '60%'),
-                                                   br(), p('press mic to begin recording'),
-                                                   uiOutput('mic')
-                                                 )
-                                        ),
-                                        tabPanel('upload', 
-                                                 br(),
-                                                 p('someday soon'))
-                                      )
-                      ),
-                      bsCollapsePanel('3. personalize', uiOutput('personalize')),
-                      bsCollapsePanel('4. details', uiOutput('details_card')),
-                      bsCollapsePanel('review', uiOutput('checkout'))
-           ),
-           helpText(
-             'comments/questions?', 
-             a('contact amorata', href = 'mailto:info@amoratadesigns.com')
-           )
-    )
-  )
-)
+      h1('W A V E S'),
+      bsCollapse(
+        id = "main", 
+        open = "intro",
+        bsCollapsePanel(
+          'intro',
+          img(src = 'eddie-intro3.jpg', align = 'center', width = '100%', height = 'auto')
+        ),
+        bsCollapsePanel(
+          '1. graphic size',
+          p('select between an across the chest graphic (11"x6") or a left pocket icon (3"x1.6")'),
+          radioButtons_withHTML('image_size', NULL, choices = image_sizes, inline = TRUE)
+        ),
+        bsCollapsePanel(
+          '2. create',
+          tabsetPanel(
+            tabPanel(
+              'record',
+              fluidRow(
+                imageOutput('sample_wave', height = 'auto', width = '60%'),
+                br(), p('press mic to begin recording'),
+                uiOutput('mic')
+              )
+            ),
+            tabPanel(
+              'upload', 
+              br(),
+              p('someday soon')
+            )
+          )
+        ),
+        bsCollapsePanel('3. personalize', uiOutput('personalize')),
+        bsCollapsePanel('4. details', uiOutput('details_card')),
+        bsCollapsePanel('review', uiOutput('checkout'))
+      ), # end of bsCollapse
+      helpText(
+        'comments/questions?', 
+        a('contact amorata', href = 'mailto:info@amoratadesigns.com')
+      )
+    ) # end of centering column
+  ) # end of main fluidRow
+) # end of everything
