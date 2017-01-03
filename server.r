8a7df70736ee43dc1ed45564bc60a1dcebd06bc7
Andres Fonseca <theandresfonseca@gmail.com>
2017-01-03 17:10:56 +0000

    card format update

--- server.r
+++ server.r
@@ -1,5 +1,5 @@
 source("~/apps/waves/util.r")
- 
+
 function(input, output, session) { 
   
   #Plot Parameters
@@ -15,8 +15,9 @@ function(input, output, session) {
     pp$fl  <- input$fl
     pp$lw  <- input$lw
     pp$ss  <- input$shirt_size
-    pp$sc  <- input$shirt_color
   })
+  
+  ### CREATE ###
   
   observeEvent(input$audio, {
     x$mp3 <- NULL
@@ -25,54 +26,128 @@ function(input, output, session) {
     
     tfile <- tempfile(fileext='.wav')
     writeBin(audio, tfile)
-
+    
     x$mp3 <- tuneR::readWave(tfile)
     file.remove(tfile)
   })
   
+  output$mic <- mic_ui
+  
+  output$sample_wave <- renderImage({
+    validate(need(x$mp3, ''))
+    
+    w = session$clientData$output_sample_wave_width; h = w*(2/3)
+    outfile = tempfile(fileext='.png')
+    
+    # Generate a png
+    png(file = outfile, width = 1200, height = 800, res = 120)
+    sample_plot(x$mp3,  input$image_size)
+    dev.off()
+    
+    # Return a list
+    list(src = outfile,
+         width  = w,
+         height = h,
+         alt = "trouble loading image")
+  }, deleteFile = TRUE)
+  
+  ### PERSONALIZE ###
+  
   output$control_panel <- renderUI({
-    validate(need(input$image_size, ''))
     validate(need(x$mp3, ''))
     if (input$image_size == 'across chest 11"x6"') big_control_panel()
     else if (input$image_size == 'left pocket 3"x1.6"') small_control_panel()
   })
   
-  output$button <- renderUI({
+  output$wave <- renderImage({
     validate(need(input$image_size, ''))
     validate(need(x$mp3, ''))
-    validate(need(input$shirt_size, 'choose a shirt size'))
-    validate(need(input$shirt_color, 'choose a shirt color'))
-    actionButton('order', "PROCEED TO CHECKOUT")
-  })
-  
-  output$wave <- renderPlot({
-    validate(need(input$image_size, ''))
-    validate(need(x$mp3, ''))
-    validate(need(input$detail, ''))
     validate(need(input$hex, ''))
     if (input$image_size == 'across chest 11"x6"') validate(need(input$lw, ''))
+    
+    w = session$clientData$output_wave_width; h = w*(2/3)
+    outfile = tempfile(fileext='.png')
+    
+    # Generate a png
+    png(file = outfile, width = 1200, height = 800, res = 120)
     plot_wave(reactiveValuesToList(pp), x$mp3, input$image_size) 
-  }, height = function() {
-    (session$clientData$output_wave_width)*(2/3)
-  })
+    dev.off()
+    
+    # Return a list
+    list(src = outfile,
+         width  = w,
+         height = h,
+         alt = "trouble loading image")
+  }, deleteFile = TRUE)
   
-  output$mic <- renderUI({
-    validate(need(input$image_size, ''))
-    tags$div(
-      tags$div(
-        id = 'viz',
-        HTML("
-          <canvas id=\"analyser\" width=\"0\" height=\"0\"></canvas>
-          <canvas id=\"wavedisplay\" width=\"0\" height=\"0\"></canvas>")),
-      tags$div(
-        id = 'controls',
-        HTML("
-          <img id=\"record\" src=\"mic128.png\" onclick=\"toggleRecording(this);\">
-          <a id=\"save\" href=\"#\"><img src=\"save.svg\"></a>")),
-      br()
+  output$personalize <- renderUI({
+    validate(need(x$mp3, 'record/upload an mp3'))
+    fluidRow(
+      column(9, imageOutput('wave', height = 'auto', width = '90%')),
+      column(3, uiOutput('control_panel'))
     )
   })
-
+  
+  observeEvent(input$detail, {
+    updateSelectInput(session, 'lw', choices = lw_choices(input$detail))
+  })
+  
+  ### DETAILS ###
+  
+  # track the number of input boxes to render
+  counter <- reactiveValues(n = 1)
+  
+  observeEvent(input$add_btn, {counter$n <- counter$n + 1})
+  observeEvent(input$rm_btn, {if (counter$n > 1) counter$n <- counter$n - 1})
+  
+  output$details <- renderUI({
+    validate(need(x$mp3, 'record/upload an mp3'))
+    lapply(seq_len(counter$n), function(x) render_detail_row(x))
+  })
+  
+  order_details <- reactive({
+    sapply(seq_len(counter$n), function(i) {
+      q  = input[[paste0('q', i)]]
+      ss = input[[paste0('shirt_size', i)]]
+      sc = input[[paste0('shirt_color', i)]]
+      paste0('(', paste(q, ss, sc, sep = ' - '),  ')')
+    })
+  })
+  
+  total_quantity <- reactive({
+    qs = sapply(seq_len(counter$n), function(i) {
+      as.numeric(input[[paste0('q', i)]])
+    })
+    sum(qs)
+  })
+  
+  output$od_test <- renderPrint({order_details()})
+  output$tq_test <- renderPrint({total_quantity()})
+  output$an_test <- renderPrint({no_null_details()})
+  
+  output$details_card <- renderUI({
+    validate(need(x$mp3, 'record/upload an mp3'))
+    fluidRow(
+      p('select shirt color, size, and quantity'),
+      column(3,
+             fluidRow(
+               column(6, actionButton('add_btn', NULL, icon = icon('plus'), width = '100%')),
+               column(6, actionButton('rm_btn', NULL, icon = icon('minus'),  width = '100%'))
+             )
+      ),
+      column(9, uiOutput('details')),
+      fluidRow(
+        column(6, offset = 3,
+               fluidRow(
+                 column(4, actionLink('example', 'samples')),
+                 column(4, a('size guide', href = 'https://www.amoratadesigns.com/pages/size-guide', target = '_blank')),
+                 column(4, a('bundles', href = 'https://www.amoratadesigns.com/pages/bundles',  target = '_blank'))
+               )
+        )
+      )
+    )
+  })
+  
   observeEvent(input$example, {
     showModal(modalDialog(
       title = "samples",
@@ -80,11 +155,77 @@ function(input, output, session) {
       easyClose = TRUE,
       size = 'l'
     ))
-  }) 
-
-  onclick("order", {
-    fn = plot_wave(reactiveValuesToList(pp), x$mp3, input$image_size, TRUE) 
-    js$order(mk_url(fn, input$shirt_size, input$shirt_color, input$image_size, input$q))
   })
-      
+  
+  no_null_details <- reactive({
+    ad = sapply(seq_len(counter$n), function(i) {
+      x = isTruthy(input[[paste0('q', i)]])
+      y = isTruthy(input[[paste0('shirt_size', i)]])
+      z = isTruthy(input[[paste0('shirt_color', i)]])
+      c(x, y, z)
+    })
+    all(ad)
+  })
+  
+  ### CONFIRMATION ###
+  
+  output$wave_confirmation <- renderImage({
+    validate(need(input$image_size, ''))
+    validate(need(x$mp3, ''))
+    
+    w = session$clientData$output_wave_confirmation_width; h = w*(2/3)
+    outfile = tempfile(fileext='.png')
+    
+    # Generate a png
+    png(file = outfile, width = 1200, height = 800, res = 120)
+    plot_wave(reactiveValuesToList(pp), x$mp3, input$image_size) 
+    dev.off()
+    
+    # Return a list
+    list(src = outfile,
+         width  = w,
+         height = h,
+         alt = "trouble loading image")
+  }, deleteFile = TRUE)
+  
+  output$details_summary <- renderTable({
+    y = c(paste(total_quantity(), 'shirts (total)'), order_details())
+    x = c('order summary:', rep('', length(y) - 1))
+    data.table(x = x, y = y)
+  }, align = 'cc', colnames = FALSE, bordered = FALSE, striped = FALSE)
+  
+  output$checkout <- renderUI({
+    validate(need(x$mp3, 'record/upload an mp3'))
+    validate(need(no_null_details(), 'please fill all fields in step 4'))
+    fluidRow(
+      em('heads up, what you see here will be printed. what do you want to do?'),
+      imageOutput('wave_confirmation', height = 'auto', width = '60%'),
+      br(),
+      fluidRow(
+        tableOutput('details_summary'),
+        column(8, offset = 2,
+               fluidRow(
+                 column(6, actionButton('order', "PROCEED TO CHECKOUT")),
+                 column(6, actionButton('open_personalize', "CONTINUE PERSONALIZING"))
+               )
+               # ,verbatimTextOutput('od_test')
+               # ,verbatimTextOutput('tq_test')
+               # ,verbatimTextOutput('url_test')
+               # ,verbatimTextOutput('an_test')
+        )
+      )
+    )
+  })
+  
+  output$url_test <- renderPrint({mk_url('idk.png', order_details(), total_quantity())})
+  
+  observeEvent(input$order, {
+    fn = plot_wave(reactiveValuesToList(pp), x$mp3, input$image_size, TRUE)
+    js$order(mk_url(fn, order_details(), total_quantity(),  input$image_size))
+  })
+  
+  observeEvent(input$open_personalize, {
+    updateCollapse(session, 'main', open = '3. personalize', close = 'review')
+  })
+  
 }
--- ui.r
+++ ui.r
@@ -2,53 +2,58 @@ source("~/apps/waves/util.r")
 
 df = c('#1b87e0','#ed635f','#6abf90','#ff814a','#8369a8','#f4de5b')
 waves_id = 21393505797
-shirt_sizes  = c('xs','s','m', 'l', 'xxl', 'xxxl')
-shirt_colors = c('white','ash', 'black')
-image_sizes  = c('left pocket 3"x1.6"', 'across chest 11"x6"')
+shirt_sizes  = c('xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl')
+shirt_colors = c('white', 'ash', 'black')
+image_sizes  = c('<img src="gs_ac.jpg">' = 'across chest 11"x6"', '<img src="gs_lp.jpg">' = 'left pocket 3"x1.6"')
 
 jsCode <- "
-  shinyjs.order = function(params) {
-    //console.log(params[0]);
-    window.location.href = params[0];
-  }"
+shinyjs.order = function(params) {
+//console.log(params[0]);
+window.location.href = params[0];
+}"
 
 fluidPage(
+  
   title = 'W A V E S',
   theme = "adjust.css",
   
   useShinyjs(),
   extendShinyjs(text = jsCode, functions = c('order')),
-  head_html, 
+  head_html,
   
   fluidRow(
-    column(3, align = 'center',
-      fluidRow(column(12, 
-        h1('W A V E S'),
-        p('select a graphic size, press the mic to record, then personalize. 
-           this DV only works on desktop (except for android chrome)'),
-        selInput('image_size',  image_sizes, 'choose graphic size'),
-        uiOutput('mic')
-      )),
-      
-      uiOutput('control_panel'),
-      
-      fluidRow(column(12, align = 'center',
-        selInput('shirt_size',  shirt_sizes, 'shirt size'),
-        selInput('shirt_color', shirt_colors, 'shirt color'),
-        numericInput('q', 'quantity', 1, min = 1, step = 1)
-      )),
-      fluidRow(column(12, align = 'center',
-        actionLink('example', 'samples'), hr(),
-        uiOutput('button')
-      )),
-      hr(),
-      helpText(
-        'comments, questions, issues?',
-        a('info@amoratadesigns.com', href = 'mailto:info@amoratadesigns.com')
-      )
-    ),
-    column(9, align = 'center',
-      plotOutput('wave', height = 'auto', width = '90%')
+    column(8, offset = 2, align = 'center',
+           h1('W A V E S'),
+           bsCollapse(id = "main", open = "intro",
+                      bsCollapsePanel('intro',
+                                      img(src = 'eddie-intro3.jpg', align = 'center', width = '100%', height = 'auto')
+                      ),
+                      bsCollapsePanel('1. graphic size',
+                                      p('select between an across the chest graphic (11"x6") or a left pocket icon (3"x1.6")'),
+                                      radioButtons_withHTML('image_size', NULL, choices = image_sizes, inline = TRUE)
+                      ),
+                      bsCollapsePanel('2. create',
+                                      tabsetPanel(
+                                        tabPanel('record',
+                                                 fluidRow(
+                                                   imageOutput('sample_wave', height = 'auto', width = '60%'),
+                                                   br(), p('press mic to begin recording'),
+                                                   uiOutput('mic')
+                                                 )
+                                        ),
+                                        tabPanel('upload', 
+                                                 br(),
+                                                 p('someday soon'))
+                                      )
+                      ),
+                      bsCollapsePanel('3. personalize', uiOutput('personalize')),
+                      bsCollapsePanel('4. details', uiOutput('details_card')),
+                      bsCollapsePanel('review', uiOutput('checkout'))
+           ),
+           helpText(
+             'comments/questions?', 
+             a('contact amorata', href = 'mailto:info@amoratadesigns.com')
+           )
     )
   )
 )
